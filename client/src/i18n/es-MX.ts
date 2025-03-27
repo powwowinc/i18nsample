6158da8435cd3c4bb5a58120346a2cdb488524d8
Jonathan Kaplan <jonathan.kaplan@powwowmobile.com>
2025-03-27 15:41:07 -0700

    [Studio] Update source of file "client/src/i18n/es-MX.ts"

--- client/src/i18n/es-MX.ts
+++ client/src/i18n/es-MX.ts
@@ -1,8 +1,8 @@
 export const es_MX = {
-    "Company Name": "MX Company Name",
-    "Username": "MX Username",
-    "Password": "MX Password",
-    "Home": "MX Home",
-    "homeHeader": "Our MX mission is to deliver great personal service and the best financial products to our customers – and the local communities we serve.",
-    "Web<br>Resources": "MX Web <br> Resources"
+    "Company Name": "Nombre de la Empresa",
+    "Username": "Nombre de Usuario",
+    "Password": "Contraseña",
+    "Home": "Inicio",
+    "homeHeader": "Nuestra misión es ofrecer un excelente servicio personal y los mejores productos financieros a nuestros clientes y a las comunidades locales a las que servimos.",
+    "Web<br>Resources": "Recursos <br> Web"
 };
