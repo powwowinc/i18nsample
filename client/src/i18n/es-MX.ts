086c71c929677bd8fd8948240c4401b14369d9df
SmartUX Admin <admin@smartux.com>
2025-03-27 15:14:39 -0700

    [Studio] Reverted to "[Studio] Modified screen "HomeIconClassic""

---
+++ client/src/i18n/es-MX.ts
@@ -0,0 +1,8 @@
+export const es_MX = {
+    "Company Name": "MX Company Name",
+    "Username": "MX Username",
+    "Password": "MX Password",
+    "Home": "MX Home",
+    "Our mission is to deliver great personal service and the best financial products to our customers – and the local communities we serve.": "Our MX mission is to deliver great personal service and the best financial products to our customers – and the local communities we serve.",
+    "Web<br>Resources": "MX Web <br> Resources"
+};
