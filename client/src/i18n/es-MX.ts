8f95c45e9ab2ac1e4d96f5a4e87fa5eb32a56ca1
SmartUX Admin <admin@smartux.com>
2025-03-27 13:56:54 -0700

    [Studio] Update source of file "client/src/i18n/es-MX.ts"

--- client/src/i18n/es-MX.ts
+++ client/src/i18n/es-MX.ts
@@ -1,4 +1,8 @@
 export const es_MX = {
-    "Company Name": "Mexican Company Name",
-    "Web Resources": "MX Web<br>Resources"
+    "Company Name": "MX Company Name",
+    "Username": "MX Username",
+    "Password": "MX Password",
+    "Home": "MX Home",
+    "Our mission is to deliver great personal service and the best financial products to our customers – and the local communities we serve.": "Our MX mission is to deliver great personal service and the best financial products to our customers – and the local communities we serve.",
+    "Web Resources": "MX Web <br> Resources"
 };
