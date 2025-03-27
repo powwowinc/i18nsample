e7d19de3fbf57a0294b76129eb5ae7af06134306
Jonathan Kaplan <jonathan.kaplan@powwowmobile.com>
2025-03-27 15:22:09 -0700

    [Studio] Add screen "Choose Language"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -2,11 +2,13 @@
 import { MenuComponent } from './menu/menu';
 import { loginsimplelight_PhonePortrait } from '../pages/loginsimplelight/PhonePortrait/loginsimplelight';
 import { homeiconclassic_PhonePortrait } from '../pages/homeiconclassic/PhonePortrait/homeiconclassic';
+import { chooselanguage_PhonePortrait } from '../pages/chooselanguage/PhonePortrait/chooselanguage';
 export class Screens {
   static declarations = [
     MenuComponent,
     loginsimplelight_PhonePortrait,
-    homeiconclassic_PhonePortrait
+    homeiconclassic_PhonePortrait,
+    chooselanguage_PhonePortrait
   ];
   static mapping = {
     'loginsimplelight': {
@@ -14,6 +16,9 @@ export class Screens {
     },
     'homeiconclassic': {
       PhonePortrait: homeiconclassic_PhonePortrait
+    },
+    'chooselanguage': {
+      PhonePortrait: chooselanguage_PhonePortrait
     }
   }
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,3 +1,4 @@
+export { mock_chooselanguage as chooselanguage } from './chooselanguage/index';
 export { mock_homeiconclassic as homeiconclassic } from './homeiconclassic/index';
 export { mock_loginsimplelight as loginsimplelight } from './loginsimplelight/index';
 export { mock_menu as menu } from './menu/index';
