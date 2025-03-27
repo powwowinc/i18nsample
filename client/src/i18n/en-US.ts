2101e3a98ffc277e4189f580bbdfda76d3766cdb
SmartUX Admin <admin@smartux.com>
2025-03-27 13:19:51 -0700

    [Studio] Update source of file "client/src/i18n/en-US.ts"

--- client/src/i18n/en-US.ts
+++ client/src/i18n/en-US.ts
@@ -1,3 +1,8 @@
 export const en_US = {
-    "Company Name": "US Company Name"
+    "Company Name": "US Company Name",
+    "Username": "Username",
+    "Password": "Password",
+    "Home": "Home",
+    "Our mission is to deliver great personal service and the best financial products to our customers – and the local communities we serve.": "Our mission is to deliver great personal service and the best financial products to our customers – and the local communities we serve.",
+    "Web <br> Resources": "Wenb <br> Resources"
 };
